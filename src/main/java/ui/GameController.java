bc457a8cffdd10bcb09fcfebb8ffb621f6a0eb0c
kratikgupta77 <119071087+kratikgupta77@users.noreply.github.com>
2023-12-10 05:54:19 +0530

    Update GameController.java

--- src/main/java/ui/GameController.java
+++ src/main/java/ui/GameController.java
@@ -1,44 +1,24 @@
 package ui;
 
-//import game.Game;
-
 import javafx.scene.Group;
 import javafx.scene.Scene;
 
 public class GameController {
+    public int Hscore;
+    public int life;
+
+    public void setLife(int lifes) {
+        life = lifes;
+    }
+
+    public void setHscore(int hscore) {
+        Hscore = hscore;
+    }
+
     public GameUI gameUI;
-    //  private Game game;
-
-
-    public GameController() {
-        //this.gameUI = new GameUI(this);
-        //  this.game=new Game();
-        initializeGame();
+    public void startNewGame(Scene scene, Group group,int rec,int life) {
+        gameUI = new GameUI(scene, group,this);
+        this.Hscore=rec;
     }
-
-    private void initializeGame() {
-        // Initialize your game logic here
-
-        // For example, you might want to start the game loop or handle user input
-    }
-
-    public void startNewGame(Scene scene, Group group) {
-//        game = new Game();
-
-        // Create a new instance of GameUI
-        gameUI = new GameUI(scene, group);
-
-        // Make sure to initialize the UI if needed
-        // gameUI.initializeUI();
-    }
-
-
-    public GameUI getGameUI() {
-        return gameUI;
-    }
-    //  public Game getGame(){
-    //return game;
 }
-// Add methods to handle user input and game logic
-
 
